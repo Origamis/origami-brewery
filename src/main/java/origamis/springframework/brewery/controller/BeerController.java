c64db4fdcb771a2b27a516532ac4de53e73dfc04
andrius.antipovas <andrius.antipovas@visma.com>
2025-05-29 11:26:12 +0300

    Fixed path

--- src/main/java/origamis/springframework/brewery/controller/BeerController.java
+++ src/main/java/origamis/springframework/brewery/controller/BeerController.java
@@ -5,21 +5,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import origamis.springframework.brewery.domain.BeerDto;
 import origamis.springframework.brewery.service.BeerService;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/beer/")
+@RequestMapping("/api/v1/beer")
 @AllArgsConstructor
 @Slf4j
 public class BeerController {
     
     private final BeerService beerService;
     
-    @GetMapping("{beerId}")
+    @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
         return ResponseEntity.ok(beerService.getBeerById(beerId));
     }
@@ -35,14 +43,14 @@ public class BeerController {
         return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
     }
 
-    @PutMapping("{beerId}")
+    @PutMapping("/{beerId}")
     public ResponseEntity<Void> updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
         return ResponseEntity.noContent().build();
     }
     
-    @DeleteMapping("{beerId}")
+    @DeleteMapping("/{beerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeerById(@PathVariable UUID beerId) {
         log.info("Deleting beer with id {}", beerId);
